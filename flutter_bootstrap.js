ec51ed9cc38db2f2817d7d364979db1ea8d6d5e3
Pablo Biagioli <pabiagioli@gmail.com>
2025-01-29 15:48:45 -0300

    fixes in search and added a progress bar

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -27,6 +27,6 @@ _flutter.loader.load({
   },
   serviceWorkerSettings: {
     serviceWorkerUrl: currentPath+"flutter_service_worker.js",
-    serviceWorkerVersion: "4286472737",
+    serviceWorkerVersion: "2739842483",
   }
 });
