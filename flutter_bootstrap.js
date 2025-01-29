5ff30e39f23b32aae84b3d672c75fbac1e733a59
Pablo Biagioli <pabiagioli@gmail.com>
2025-01-29 14:22:04 -0300

    fixes for colors and typo in the DB

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -27,6 +27,6 @@ _flutter.loader.load({
   },
   serviceWorkerSettings: {
     serviceWorkerUrl: currentPath+"flutter_service_worker.js",
-    serviceWorkerVersion: "3172659407",
+    serviceWorkerVersion: "4286472737",
   }
 });
