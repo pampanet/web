03a9a9668d15d6cbf074feb016d50e0df6ed1c5b
Pablo Biagioli <pabiagioli@gmail.com>
2025-03-19 10:27:32 -0300

    manually added toastification

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -27,6 +27,6 @@ _flutter.loader.load({
   },
   serviceWorkerSettings: {
     serviceWorkerUrl: currentPath+"flutter_service_worker.js",
-    serviceWorkerVersion: "239572545",
+    serviceWorkerVersion: "3593609645",
   }
 });
